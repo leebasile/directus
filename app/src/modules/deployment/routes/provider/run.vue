ebe4b051c525ec8ca7e6282ee6935d25350e0a76
Lindsey Zylstra <lindseyzylstra@gmail.com>
2026-04-17 12:13:11 -0700

    Add Cloudflare deployments

--- app/src/modules/deployment/routes/provider/run.vue
+++ app/src/modules/deployment/routes/provider/run.vue
@@ -8,7 +8,7 @@ import { useI18n } from 'vue-i18n';
 import DeploymentStatus from '../../components/deployment-status.vue';
 import DeploymentNavigation from '../../components/navigation.vue';
 import { useDeploymentNavigation } from '../../composables/use-deployment-navigation';
-import { useProviderConfigs } from '../../config/providers';
+import { formatDeploymentTargetLabel, useProviderConfigs } from '../../config/providers';
 import VBreadcrumb from '@/components/v-breadcrumb.vue';
 import VButton from '@/components/v-button.vue';
 import VCardActions from '@/components/v-card-actions.vue';
@@ -36,6 +36,9 @@ const props = defineProps<{
 
 const { t } = useI18n();
 const { currentProject, providers } = useDeploymentNavigation();
+
+const deploymentTargetLabel = (target: string) => formatDeploymentTargetLabel(target, t);
+
 const { providerConfigs } = useProviderConfigs();
 
 const canCancel = usePermissionsStore().hasPermission('directus_deployment_runs', 'update');
@@ -355,7 +358,7 @@ onUnmounted(() => {
 				<div class="stat-card">
 					<VIcon name="assignment" class="stat-icon" />
 					<span class="stat-label">{{ $t('deployment.target') }}</span>
-					<span class="stat-value">{{ $t(`deployment.target_value.${run.target}`) }}</span>
+					<span class="stat-value">{{ deploymentTargetLabel(run.target) }}</span>
 				</div>
 			</div>
 
